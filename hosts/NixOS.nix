44a11959d759a3f5c7c090fbb278fd26be90b412
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2021-10-06 21:49:52 +0200

    This probably works

--- hosts/NixOS.nix
+++ hosts/NixOS.nix
@@ -9,43 +9,51 @@
     "${nixosModulesPath}/installer/sd-card/sd-image-aarch64.nix"
   ];
 
+  security.sudo.wheelNeedsPassword = false;
+
   boot.supportedFilesystems = lib.mkForce [ "vfat" "ext4" ];
 
   services.openssh = {
     openFirewall = true;
   };
-  
+
   # https://github.com/NixOS/nixpkgs/issues/10001
   networking = {
     useDHCP = false;
+    dhcpcd.enable = false;
     useNetworkd = true;
   };
+  systemd.network.enable = true;
+  networking.usePredictableInterfaceNames = lib.mkForce true;
 
-  systemd.network.networks = let
-    networkConfig = {
-      DHCP = "yes";
-      DNSSEC = "yes";
-      DNSOverTLS = "yes";
-      DNS = [ "1.1.1.1" "1.0.0.1" ];
+  systemd.network.networks =
+    let
+      networkConfig = {
+        DHCP = "yes";
+        # resolvectl dnssec eth0 off # to fix
+        DNSSEC = "false"; # can't use because NTP doesn't get bootstrapped - tihs is supposed to work but it doesn't
+        DNSOverTLS = "false";
+        DNS = [ "1.1.1.1" "1.0.0.1" ];
+      };
+    in
+    {
+      # Config for all useful interfaces
+      "40-wired" = {
+        enable = true;
+        name = "e*";
+        inherit networkConfig;
+        dhcpV4Config.RouteMetric = 1024; # Better be explicit
+      };
+      "40-wireless" = {
+        enable = true;
+        name = "w*";
+        inherit networkConfig;
+        dhcpV4Config.RouteMetric = 2048; # Prefer wired
+      };
     };
-  in {
-    # Config for all useful interfaces
-    "40-wired" = {
-      enable = true;
-      name = "en*";
-      inherit networkConfig;
-      dhcpV4Config.RouteMetric = 1024; # Better be explicit
-    };
-    "40-wireless" = {
-      enable = true;
-      name = "wl*";
-      inherit networkConfig;
-      dhcpV4Config.RouteMetric = 2048; # Prefer wired
-    };
-  };
 
   # Wait for any interface to become available, not for all
-  systemd.services."systemd-networkd-wait-online".serviceConfig.ExecStart = [
-    "" "${config.systemd.package}/lib/systemd/systemd-networkd-wait-online --any"
-  ];
+  # systemd.services."systemd-networkd-wait-online".serviceConfig.ExecStart = [
+  #   "" "${config.systemd.package}/lib/systemd/systemd-networkd-wait-online --any"
+  # ];
 }
--- hosts/bootstrap.nix
+++ hosts/bootstrap.nix
@@ -15,11 +15,55 @@
     "${nixosModulesPath}/installer/sd-card/sd-image-aarch64-installer.nix"
   ];
 
+  sdImage.compressImage = false;
+
+  security.sudo.wheelNeedsPassword = false;
+
   boot.supportedFilesystems = lib.mkForce [ "vfat" "ext4" ];
 
   services.openssh = {
     openFirewall = true;
   };
+
+  # https://github.com/NixOS/nixpkgs/issues/10001
+  networking = {
+    useDHCP = false;
+    dhcpcd.enable = false;
+    useNetworkd = true;
+  };
+  systemd.network.enable = true;
+  networking.usePredictableInterfaceNames = lib.mkForce true;
+
+  systemd.network.networks =
+    let
+      networkConfig = {
+        DHCP = "yes";
+        # resolvectl dnssec eth0 off # to fix
+        DNSSEC = "false"; # can't use because NTP doesn't get bootstrapped - tihs is supposed to work but it doesn't
+        DNSOverTLS = "false";
+        DNS = [ "1.1.1.1" "1.0.0.1" ];
+      };
+    in
+    {
+      # Config for all useful interfaces
+      "40-wired" = {
+        enable = true;
+        name = "e*";
+        inherit networkConfig;
+        dhcpV4Config.RouteMetric = 1024; # Better be explicit
+      };
+      "40-wireless" = {
+        enable = true;
+        name = "w*";
+        inherit networkConfig;
+        dhcpV4Config.RouteMetric = 2048; # Prefer wired
+      };
+    };
+
+  # Wait for any interface to become available, not for all
+  # systemd.services."systemd-networkd-wait-online".serviceConfig.ExecStart = [
+  #   "" "${config.systemd.package}/lib/systemd/systemd-networkd-wait-online --any"
+  # ];
 
   # nix-shell -p raspberrypi-eeprom
   # mount /dev/disk/by-label/FIRMWARE /mnt
